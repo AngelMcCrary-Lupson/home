a60d71fac99fcfad956dc9da1420148b2bcb2bf4
AngelMcCraryLupson <AngelMcCraryLupson@users.noreply.github.com>
2020-12-23 11:13:13 -0800

    Added new projects

--- src/containers/OtherProjects.js
+++ src/containers/OtherProjects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './OtherProjects.css';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import exlink from '../Ex-Link.png';
-
+// To deploy https://dev.to/yuribenjamin/how-to-deploy-react-app-in-github-pages-2a1f
 class OtherProjects extends Component {
   render() {
     return (
@@ -23,6 +23,42 @@ class OtherProjects extends Component {
               </Col>
               <Col xs={3}>
                 <div className="ProjBox">
+                  <h2 className="Title">Husky Study Center</h2>
+                  <p className="Desc">Virtual study space for UW students to work together like
+                                      they would in on campus study centers. Utilizes Agora’s RTC
+                                      SDK to host real time virtual calls to connect students in
+                                      the same classes.</p>
+                  <p className="Tools">Javascript, HTML, CSS, Agora RTC SDK</p>
+                  <a href="https://github.com/AngelMcCrary-Lupson/husky-study-center" target="_blank">
+                    <img className="exlink" src={exlink}/></a>
+                </div>
+              </Col>
+              <Col xs={3}>
+                <div className="ProjBox">
+                  <h2 className="Title">Playback Speed Controller</h2>
+                  <p className="Desc">Chrome Browser extension that allows users to control a video
+                                      player’s playback speed. The user can manually input the
+                                      playback speed or use buttons to adjust a video’s speed.</p>
+                  <p className="Tools">Javascript, HTML, CSS</p>
+                  <a href="https://github.com/AngelMcCrary-Lupson/video-playback-extension" target="_blank">
+                    <img className="exlink" src={exlink}/></a>
+                </div>
+              </Col>
+
+
+            </Row>
+            <Row className="Projects">
+              <Col xs={3}>
+                <div className="ProjBox">
+                  <h2 className="Title"><a href="/blackjack">Virtual Blackjack</a></h2>
+                  <p className="Desc">Web based game  of video blackjack.</p>
+                  <p className="Tools">HTML, CSS, Javascript</p>
+                  <a href="https://angelmccrary-lupson.github.io/virtual-blackjack/" target="_blank">
+                    <img className="exlink" src={exlink}/></a>
+                </div>
+              </Col>
+              <Col xs={3}>
+                <div className="ProjBox">
                   <h2 className="Title">Mixed Musical Reality</h2>
                   <p className="Desc">Created a playable virtual piano for the Hololens 2
                       to make music education interactive and fun.</p>
@@ -38,18 +74,6 @@ class OtherProjects extends Component {
                                       of music artist.</p>
                   <p className="Tools">HTML, CSS, Javascript, MusixMatch Web API</p>
                   <a href="https://angelmccrary-lupson.github.io/random-lyric-generator/" target="_blank">
-                    <img className="exlink" src={exlink}/></a>
-                </div>
-              </Col>
-
-            </Row>
-            <Row className="Projects">
-              <Col xs={3}>
-                <div className="ProjBox">
-                  <h2 className="Title"><a href="/blackjack">Virtual Blackjack</a></h2>
-                  <p className="Desc">Web based game  of video blackjack.</p>
-                  <p className="Tools">HTML, CSS, Javascript</p>
-                  <a href="https://angelmccrary-lupson.github.io/virtual-blackjack/" target="_blank">
                     <img className="exlink" src={exlink}/></a>
                 </div>
               </Col>
